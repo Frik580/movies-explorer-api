4d28f4e08e64e26dbe5e28267c691f23d68f22ae
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2022-09-20 20:55:42 +0300

    Инициализация

---
+++ app.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { errors, celebrate, Joi } = require('celebrate');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { createUser, login } = require('./contollers/users');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { NotFound } = require('./errors/notfound');
+const { urlPattern } = require('./utils/constants');
+
+const app = express();
+
+app.use(cors());
+
+const { PORT = 3001 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(express.json());
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post(
+  '/api/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(urlPattern),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
+app.post(
+  '/api/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+app.use(auth);
+
+app.use('/api/', usersRouter);
+app.use('/api/', cardsRouter);
+app.use('*', () => {
+  throw new NotFound('Страница не найдена');
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ message: err.message });
+  } else {
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
